ccbfe22b3aa10087ffade0f5807721e16a045b9a
code0wl <o.lodriguez@gmail.com>
2018-04-22 11:55:50 +0200

    clean up

--- src/modules/components/theme/particle.class.ts
+++ src/modules/components/theme/particle.class.ts
@@ -3,16 +3,17 @@ import { LocationService } from "../location/location.class";
 import AnimationLoop from "../engine/engine.class";
 import { Theme } from "./theme.model";
 
-declare const pusherColor: any;
-
 export class Particle implements Theme {
 
     public sphere: THREE.Mesh;
 
     private globe: THREE.SphereGeometry;
     private clouds: THREE.Mesh;
+    private count: number;
 
-    constructor(scene: Scene) {
+    constructor(private scene: Scene) {
+
+
 
     }
 
